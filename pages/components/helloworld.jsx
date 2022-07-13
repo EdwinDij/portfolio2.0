1dc0d255f09ede123e76528f048e3bd7b844f148
EdwinDij <edwin.d899@gmail.com>
2022-07-13 09:25:16 +0200

    background léger a mettre premiere page terminé sans les routes

--- pages/components/helloworld.jsx
+++ pages/components/helloworld.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import {React, Link} from 'react'
 
 export default function helloworld() {
     return (
         <div className='helloworld'>
             <div className="title-zone">
                 <h1 className="hook">
-                    Développer, Intégrer, Concevoir, Améliorer.
+                    <span className='letter-color'>D</span>évelopper, 
+                    <span className='letter-color'> I</span>ntégrer, 
+                    <span className='letter-color'> C</span>oncevoir, <br></br> 
+                    <span className='letter-color'> A</span>méliorer.
                 </h1>
+                <div className="getIn">
+                    <p className='getIn-text'> Jump in ! </p>
+                </div>
             </div>
-            <h2>bonjour</h2>
         </div>
     )
 }
