2ff26cf01b899818a5fe9e22feeae3820e2cdeee
EdwinDij <edwin.d899@gmail.com>
2022-07-14 02:42:48 +0200

    api retour en arriere

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -3,7 +3,7 @@ const mail = require('@sendgrid/mail');
 
 
 export default  async function sendMail(req, res) {
-  mail.setApiKey("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");
+  mail.setApiKey(process.env.SENDGRID_API_KEY);
 
   const body = JSON.parse(req.body);
   const message = `
