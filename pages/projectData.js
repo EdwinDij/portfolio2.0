b4eb4cd0e6400e49d93016bf89b62d7c65c50c3a
EdwinDij <edwin.d899@gmail.com>
2022-08-06 03:01:25 +0200

    project data, creation d'un tableau d'object pour les projets

---
+++ pages/projectData.js
@@ -0,0 +1,95 @@
+import Reservia from '../public/reservia.png';
+import Ohmyfood from '../public/ohmyfood.png';
+import ChouetteAgence from '../public/chouette-agence.png';
+import Kanap from '../public/kanap.png';
+import Piiquante from '../public/piiquante.png';
+import groupomania from '../public/groupomania.png';
+import todoList from '../public/todoList.png';
+import smoosh from '../public/smoosh.png';
+import zephyr from '../public/zephyr.png';
+
+export const projectItems = [
+    {
+        id: 1,
+        title: 'Réservia',
+        image: Reservia,
+        icons: "",
+        role: "Intégration front-end",
+        alt: "img project",
+        link: "https://edwindij.github.io/version-avant-29-09-21-EdwinDijeont_2_31102021/"
+    },
+    {
+        id: 2,
+        title: 'OhmyFood',
+        image: Ohmyfood,
+        icons: "",
+        role: "Intégration front-end",
+        alt: "img project",
+        link: "https://edwindij.github.io/EdwinDijeont_3_26112021/"
+    },
+    {
+        id: 3,
+        title: 'La Chouette Agence',
+        image: ChouetteAgence,
+        icons:"",
+        role: "SEO",
+        alt: "img project",
+        link: "https://github.com/EdwinDij/EdwinDijeont_4_22012022"
+    },
+    {
+        id: 4,
+        title: 'Kanap',
+        image: Kanap,
+        icons:"",
+        role: "Front-end",
+        alt: "img project",
+        link: "https://github.com/EdwinDij/EdwinDijeont_5_13022022-"
+    },
+    {
+        id: 5,
+        title: 'Piiquante',
+        image:  Piiquante,
+        icons:"",
+        role: "Back-end",
+        alt: "img project",
+        link: "https://github.com/EdwinDij/EdwinDij-EdwinDijeont_6_03032022--"
+    },
+    {
+        id: 6,
+        title: "Groupomania",
+        image: groupomania,
+        icons:"",
+        role: "Full-stack",
+        alt: "img project",
+        link: "https://github.com/EdwinDij/dijeont_edwin_n7_groupomania_020522"
+    },
+    {
+        id: 7,
+        title: "Todo-List",
+        image: todoList,
+        icons:"",
+        role: "Front-end",
+        alt: "img project",
+        link: "https://github.com/EdwinDij/todolist"
+    },
+    {
+        id: 8,
+        title: "Smosh",
+        image: smoosh,
+        icons:"",
+        role: "Full-stack",
+        alt: "img project",
+        link: "https://github.com/EdwinDij/Smoosh",
+        status: "En cours"
+    },
+    {
+        id: 9,
+        title: "Zephyr Esport",
+        image: zephyr,
+        icons:"",
+        role: "Front-end",
+        alt: "img project",
+        status: "Ce projet en cours ne peut pas être affiché"
+    }
+
+]
--- pages/projects.jsx
+++ pages/projects.jsx
@@ -6,140 +6,26 @@ import Link from 'next/link'
 export default function projects() {
   return (
     <motion.div
-    initial={{y:200, opacity:0}}
-    animate={{y:0, transition:4, opacity:1}}
-    exit={{scale:0.2, opacity:0, transition:5}}>
+      initial={{ y: 200, opacity: 0 }}
+      animate={{ y: 0, transition: 4, opacity: 1 }}
+      exit={{ scale: 0.2, opacity: 0, transition: 5 }}>
       <h1 className='title-project'> Projets</h1>
       <div className="project-container">
         <div className="project">
-          <figure className="snip1504">
-            <Image className='img-project'
-              src={"/reservia.png"}
-              width={260}
-              height={260}
-              alt="sample57" />
-            <figcaption>
-              <h3>Réservia</h3>
-              <div className="flex-icon">
-                <Image className='icon-project'
-                  src={'/html.png'}
-                  width={50}
-                  height={50}
-                  alt="icone tech"
-                />
-                <Image className='icon-project'
-                  src={'/css.png'}
-                  width={50}
-                  height={50}
-                  alt="icone tech"
-                />
-              </div>
-            </figcaption>
-            <a href="https://edwindij.github.io/version-avant-29-09-21-EdwinDijeont_2_31102021/" target="_blank" rel="noopener noreferrer"></a>
-          </figure>
 
-          <figure className="snip1504">
-            <Image className='img-project'
-              src={"/ohmyfood.png"}
-              width={260}
-              height={260}
-              alt="sample57" />
-            <figcaption>
-              <h3>Ohmyfood</h3>
-              <div className="flex-icon">
-                <Image className='icon-project'
-                  src={'/html.png'}
-                  width={50}
-                  height={50}
-                  alt="icone tech"
-                />
-                <Image className='icon-project'
-                  src={'/css.png'}
-                  width={50}
-                  height={50}
-                  alt="icone tech"
-                />
+          <div className="project-card">
+            <div className="project-card-image">
+              <Image className='img-project'
+                src={"/reservia.png"}
+                width={450}
+                height={350}
+                alt="reservia" />
+                <div className="project-card-text">
+                <h2 className="project-card-title">Reservia</h2>
               </div>
-            </figcaption>
-            <a href="https://edwindij.github.io/EdwinDijeont_3_26112021/" target="_blank" rel="noopener noreferrer"></a>
-          </figure>
-
-          <figure className="snip1504">
-            <Image className='img-project'
-              src={"/kanap.png"}
-              width={260}
-              height={260}
-              alt="sample57" />
-            <figcaption>
-              <h3>Kanap</h3>
-              <div className="flex-icon">
-                <Image className='icon-project'
-                  src={'/javascript.png'}
-                  width={50}
-                  height={50}
-                  alt="icone tech"
-                />
-              </div>
-            </figcaption>
-            <a href="https://github.com/EdwinDij/EdwinDijeont_5_13022022-" target="_blank" rel="noopener noreferrer"></a>
-          </figure>
-
-          <figure className="snip1504">
-            <Image className='img-project'
-              src={"/piquante.jpg"}
-              width={260}
-              height={260}
-              alt="sample57" />
-            <figcaption>
-              <h3>Piiquante</h3>
-              <div className="flex-icon">
-                <Image className='icon-project'
-                  src={'/javascript.png'}
-                  width={50}
-                  height={50}
-                  alt="icone tech"
-                />
-                <Image className='icon-project'
-                  src={'/nodejs.png'}
-                  width={50}
-                  height={50}
-                  alt="icone tech"
-                />
-              </div>
-            </figcaption>
-            <a href="https://github.com/EdwinDij/EdwinDij-EdwinDijeont_6_03032022--" target="_blank" rel="noopener noreferrer"></a>
-          </figure>
-          <figure className="snip1504">
-            <Image className='img-project'
-              src={"/groupomania.png"}
-              width={260}
-              height={260}
-              alt="sample57" />
-            <figcaption>
-              <h3>Groupomania</h3>
-              <div className="flex-icon">
-                <Image className='icon-project'
-                  src={'/react.png'}
-                  width={50}
-                  height={50}
-                  alt="icone tech"
-                />
-                <Image className='icon-project'
-                  src={'/nodejs.png'}
-                  width={50}
-                  height={50}
-                  alt="icone tech"
-                />
-                <Image className='icon-project'
-                  src={'/mysql.png'}
-                  width={50}
-                  height={50}
-                  alt="icone tech"
-                />
-              </div>
-            </figcaption>
-            <a href="https://github.com/EdwinDij/dijeont_edwin_n7_groupomania_020522" target="_blank" rel="noopener noreferrer"></a>
-          </figure>
+            </div>
+          </div>
+  
         </div>
       </div>
     </motion.div>
